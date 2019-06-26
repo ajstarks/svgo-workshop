c80f37a57c7794c42cdd042e9ecfe537d0acd2b6
Anthony Starks <ajstarks@gmail.com>
2019-06-26 19:05:24 -0400

    Add attribution, fix RPD code

--- code/nyt-drawtop.go
+++ code/nyt-drawtop.go
@@ -1,10 +1,10 @@
-// drawStories walks the result structure, displaying title and thumbnail
-func drawStories(canvas *svg.SVG, data NYTStories, n int) {
-	top, left := 200, 150
-	imx, titley := left-100, top-100
-	x, y, th, tw := left, top, 75, 75
-	ts := fmt.Sprintf("New York Times for %s", time.Now().Format(datefmt))
-	imagelink := ""
+// drawStories walks the result structure,
+// displaying title and thumbnail in a grid
+func drawStories(canvas *svg.SVG, width int, data NYTStories, n int) {
+	top, left := 100, 150
+	titley := top - 50
+	x, y := left, top
+	ts := fmt.Sprintf("New York Times Top Stories %s", time.Now().Format(datefmt))
 	if n > data.StoryCount {
 		n = data.StoryCount
 	}
