c9489dfe6c0151e2da1b8365d070aad67c0e3099
Anthony Starks <ajstarks@gmail.com>
2019-08-19 15:24:51 -0400

    fix const slide

--- code/nyt-const.go
+++ code/nyt-const.go
@@ -1,12 +1,12 @@
 // API Info and formats
 const (
-	NYTAPIkey = "HxTV50RYD7LyGzHxUy7XCThV3kQ3HYvk"
-	NYTfmt    = "https://api.nytimes.com/svc/topstories/v2/%s.json?api-key=%s"
-	style     = "font-size:20pt;font-family:sans-serif;fill:white"
-	errfmt    = "unable to get network data for %s (%s)"
-	headfmt   = "New York Times Top Stories for %s"
-	datefmt   = "Monday Jan 2, 2006"
-	usage     = `section choices:
+  NYTAPIkey = "HxTV50RYD7LyGzHxUy7XCThV3kQ3HYvk"
+  NYTfmt    = "https://api.nytimes.com/svc/topstories/v2/%s.json?api-key=%s"
+  style     = "font-size:10pt;font-family:sans-serif;text-anchor:middle;fill:white"
+  errfmt    = "unable to get network data for %s (%s)"
+  headfmt   = "New York Times Top Stories for %s"
+  datefmt   = "Monday Jan 2, 2006"
+  usage     = `section choices:
 arts, automobiles, books, business, fashion, 
 food, health, home, insider, magazine, movies, 
 national, nyregion, obituaries, opinion, politics, 
